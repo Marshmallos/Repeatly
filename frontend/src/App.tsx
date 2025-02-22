9b65c8f5fe1c6969e71c35542620a1f2f84a1f93
Marshmallos <oknainot@gmail.com>
2025-02-23 04:27:57 +0800

    feat: add protected routes; add function for created and authenticating user

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,30 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { Welcome } from "./pages";
+import { Login } from "./components";
 import { useState } from "react";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { AuthProvider } from "./utils/AuthProvider";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 export default function App() {
   const [queryClient] = useState(() => new QueryClient());
   return (
-    <BrowserRouter>
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <QueryClientProvider client={queryClient}>
-          <Routes>
-            <Route path="/" element={<Welcome />} />
-          </Routes>
-        </QueryClientProvider>
-      </LocalizationProvider>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <QueryClientProvider client={queryClient}>
+            <Routes>
+              <Route element={<ProtectedRoute />}>
+                <Route path="/home" element={<Welcome />} />
+              </Route>
+              <Route path="/" element={<Login />} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          </QueryClientProvider>
+        </LocalizationProvider>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
--- frontend/src/pages/Welcome.tsx
+++ frontend/src/pages/Welcome.tsx
@@ -1,10 +1,11 @@
-import { Login } from "../components";
+import { useAuth } from "../utils/useAuth";
 
 export default function Welcome() {
+  const { user } = useAuth();
   return (
     <div className="flex justify-center items-center h-screen bg-sky-500">
       {/* Enter title here */}
-      <Login />
+      Welcome {user?.username}
     </div>
   );
 }
